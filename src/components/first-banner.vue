84498ec5164ea7921f608240678d45b23fbfa994
Brian Ford <brianfordcode@gmail.com>
2021-07-27 15:39:34 -0700

    add navbar, start firrst banner

---
+++ src/components/first-banner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+      <div class="background">
+          <img class="bg-design" id="left" draggable="false" src='../assets/background/bg-design-left.png' alt="bg-design-left">
+          <img class="bg-design" id="right" draggable="false" src='../assets/main/yellow-om-trans.png' alt="yellow-om-trans">
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.container {
+    height: 100%;
+    margin: 0 auto;
+    max-width: 1600px;
+    overflow: hidden;
+}
+
+.background {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    border: 3px solid blue;
+    height: 100vh;
+}
+
+
+#left {
+    height: 70%;
+    min-height: 350px;
+    opacity: 0.25;
+}
+
+#right {
+    height: 100vh;
+    min-height: 350px;
+    margin-top: 100px;
+
+}
+
+
+</style>
---
+++ src/components/nav-bar.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+  #nav {
+      position: fixed;
+      height: 50px;
+      width: 100%;
+      background: white;
+      z-index: 50;
+  }
+
+
+</style>
